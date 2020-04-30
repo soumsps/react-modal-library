2eaa3bfbad619d0edadcc56353ca5fe85f22f2a1
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-04-30 14:54:47 +0530

    Add direct page navigation between homepage and dashboard Added direct page navigation between homepage and dashboard, also taken care if the user is not signed in and directly tries to go to dashboard then he/she will be redirected to homepage. This direct navigation flow will be changed in future after implementation of signin/ signup modal.

--- react-exercises/modal-react/src/App.js
+++ react-exercises/modal-react/src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Homepage from './pages/homepage/homepage.component';
 import Dashboard from './pages/dashboard/dashboard.component';
+import { Router } from '@reach/router';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="App">
       <div className="container">
-        <Homepage />
-        {/* <Dashboard /> */}
+        <Router>
+          <Homepage path="/" isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          <Dashboard
+            path="dashboard"
+            isLoggedIn={isLoggedIn}
+            setIsLoggedIn={setIsLoggedIn}
+          />
+        </Router>
+
         <div className="footer">Developed by Soumendu and Vinod</div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- react-exercises/modal-react/src/pages/homepage/homepage.component.jsx
+++ react-exercises/modal-react/src/pages/homepage/homepage.component.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import './homepage.styles.css';
+import { navigate } from '@reach/router';
 
-const Homepage = () => (
-  <div className="homepage-container">
-    <header role="heading" className="page-header">
-      <h1 className="page-title">Example App Homepage</h1>
-      <nav role="navigation" aria-label="Main">
-        <button className="btn btn-signin" type="button">
-          Sign in
-        </button>
-        <button className="btn btn-signup" type="button">
-          Sign up
-        </button>
-      </nav>
-    </header>
-    <div className="content-homepage">Content</div>
-  </div>
-);
+const Homepage = ({ isLoggedIn, setIsLoggedIn }) => {
+  const handleSignInButton = () => {
+    setIsLoggedIn(true);
+    navigate('/dashboard');
+  };
+  const handleSignUpButton = () => {
+    setIsLoggedIn(true);
+    navigate('/dashboard');
+  };
+  return (
+    <div className="homepage-container">
+      <header role="heading" className="page-header">
+        <h1 className="page-title">Example App Homepage</h1>
+        <nav role="navigation" aria-label="Main">
+          <button
+            className="btn btn-signin"
+            type="button"
+            onClick={() => handleSignInButton()}
+          >
+            Sign in
+          </button>
+          <button
+            className="btn btn-signup"
+            type="button"
+            onClick={() => handleSignUpButton()}
+          >
+            Sign up
+          </button>
+        </nav>
+      </header>
+      <div className="content-homepage">Content</div>
+    </div>
+  );
+};
 
 export default Homepage;
