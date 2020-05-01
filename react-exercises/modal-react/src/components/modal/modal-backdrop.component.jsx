dc3ac7adf02dc784374d864cb6eda9983b34f8f4
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-05-02 01:45:50 +0530

    Add modal close action when user click on backdrop. It has two behavior, first if backdrop is disabled by user then modal can only be closed via close button on modal or ESC button(not implemented yet). Second behavior is when backdrop is enabled then modal will close if user click on backdrop.

--- react-exercises/modal-react/src/components/modal/modal-backdrop.component.jsx
+++ react-exercises/modal-react/src/components/modal/modal-backdrop.component.jsx
@@ -2,10 +2,16 @@ import React, { useContext } from 'react';
 import ModalContext from './modal-context.component';
 
 const Backdrop = (props) => {
-  const { backdrop } = useContext(ModalContext);
+  const { backdrop, closeModalCallback } = useContext(ModalContext);
+
+  const handleCloseOnBackdropArea = () => {
+    closeModalCallback(false);
+  };
 
   return backdrop ? (
-    <div className="modal-backdrop">{props.children}</div>
+    <div className="modal-backdrop" onClick={() => handleCloseOnBackdropArea()}>
+      {props.children}
+    </div>
   ) : (
     <>{props.children}</>
   );
