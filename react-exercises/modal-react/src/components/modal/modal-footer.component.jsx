5e8b712921913eee346f1fb200dc0bab0e92d0b1
vinodkumartheking <vinodkumartheking@gmail.com>
2020-05-02 10:50:29 +0530

    Add aria tag

--- react-exercises/modal-react/src/components/modal/modal-footer.component.jsx
+++ react-exercises/modal-react/src/components/modal/modal-footer.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = (props) => {
-  return <div className="modal-footer">{props.children}</div>;
+  return <div className="modal-footer" aria-label="modal-footer">{props.children}</div>;
 };
 
 export default Footer;
