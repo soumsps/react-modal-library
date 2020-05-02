c6a9931bda237ec884cc50bb527e5d194d3353fc
vinodkumartheking <vinodkumartheking@gmail.com>
2020-05-02 13:11:49 +0530

    Add aria tag and Test cases

--- react-exercises/modal-react/src/App.test.js
+++ react-exercises/modal-react/src/App.test.js
@@ -2,9 +2,31 @@ import React from 'react';
 import { render , screen , fireEvent} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('Sign in Button present in DOM', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const signinBtn = screen.getByText(/sign in/i);
+  //fireEvent.click(signinBtn)
+
+  const SignBtn = screen.getByRole("button",{"name":"Sign In"})
+  expect(SignBtn).toBeInTheDocument();
 });
 
+test('Sign Up Button present in DOM', () => {
+  const { getByText } = render(<App />);
+  const signUpBtn = screen.getByText(/sign Up/i);
+  //fireEvent.click(signinBtn)
+
+  const SignUpBtn = screen.getByRole("button",{"name":"Sign In"})
+  expect(SignUpBtn).toBeInTheDocument();
+});
+
+
+test('modal pop up on sign in buton click', () => {
+  const { getByText } = render(<App />);
+  const signinBtn = screen.getByText(/sign in/i);
+  fireEvent.click(signinBtn)
+
+  const modalPopUp = screen.getByLabelText(/model-title/i)
+  expect(modalPopUp).toBeInTheDocument();
+});
